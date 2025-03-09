a8b2df029f1d649114052179898c58376006d82d
Vitor Gabriel <vitorgabsg@gmail.com>
2025-03-09 12:18:11 -0300

    feat: create component ui to display meal details

---
+++ src/components/meal/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Time, Separator, Description, Status } from './styles'
+
+export function Meal() {
+  return (
+    <Container>
+      <Time>20:00</Time>
+      <Separator />
+      <Description>X-tudo</Description>
+      <Status />
+    </Container>
+  )
+}
---
+++ src/components/meal/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components/native'
+
+import { mixins } from '../../styles/mixins'
+
+export const Container = styled.View`
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.colors.gray[300]};
+  border-radius: 6px;
+  padding: 12px 16px 16px;
+  flex-direction: row;
+  align-items: center;
+  gap: 12px;
+`
+
+export const Time = styled.Text`
+  ${mixins.fonts.bodyXs}
+  color: ${({ theme }) => theme.colors.gray[700]};
+`
+
+export const Separator = styled.View`
+  height: 14px;
+  width: 1px;
+  background-color: ${({ theme }) => theme.colors.gray[400]};
+`
+
+export const Description = styled.Text.attrs(() => ({
+  numberOfLines: 1,
+}))`
+  ${mixins.fonts.bodyMd}
+  color: ${({ theme }) => theme.colors.gray[600]};
+  flex-shrink: 1;
+`
+
+export const Status = styled.View`
+  background-color: ${({ theme }) => theme.colors.red.mid};
+  border-radius: 999px;
+  width: 14px;
+  height: 14px;
+  margin-left: auto;
+`
