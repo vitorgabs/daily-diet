08131958c1b34915fff3276fc72581de276820ef
Vitor Gabriel <vitorgabsg@gmail.com>
2025-03-12 18:05:29 -0300

    feat: make the component dynamic and add navigation

--- src/components/meal/index.tsx
+++ src/components/meal/index.tsx
@@ -1,12 +1,31 @@
+import { useNavigation } from '@react-navigation/native'
+import { useTheme } from 'styled-components'
+import { format } from 'date-fns'
+
+import type { Meal as MealType } from '../../store/meal'
+
 import { Container, Time, Separator, Description, Status } from './styles'
 
-export function Meal() {
+type MealProps = MealType
+
+export function Meal(props: MealProps) {
+  const { name, onDiet, consumedAt } = props
+  const { navigate } = useNavigation()
+  const { colors } = useTheme()
+
+  const formattedTime = format(consumedAt, 'HH:mm')
+
   return (
-    <Container>
-      <Time>20:00</Time>
+    <Container
+      onPress={() => navigate('Details', { meal: props })}
+      style={({ pressed }) => [
+        { backgroundColor: pressed ? colors.gray[200] : 'transparent' },
+      ]}
+    >
+      <Time>{formattedTime}</Time>
       <Separator />
-      <Description>X-tudo</Description>
-      <Status />
+      <Description>{name}</Description>
+      <Status success={onDiet} />
     </Container>
   )
 }
--- src/components/meal/styles.ts
+++ src/components/meal/styles.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components/native'
 
 import { mixins } from '../../styles/mixins'
 
-export const Container = styled.View`
+export const Container = styled.Pressable`
   background-color: transparent;
   border: 1px solid ${({ theme }) => theme.colors.gray[300]};
   border-radius: 6px;
-  padding: 12px 16px 16px;
+  padding: 14px 16px 14px 12px;
   flex-direction: row;
   align-items: center;
   gap: 12px;
@@ -30,9 +30,12 @@ export const Description = styled.Text.attrs(() => ({
   color: ${({ theme }) => theme.colors.gray[600]};
   flex-shrink: 1;
 `
+type StatusProps = {
+  success: boolean
+}
 
-export const Status = styled.View`
-  background-color: ${({ theme }) => theme.colors.red.mid};
+export const Status = styled.View<StatusProps>`
+  background-color: ${({ theme, success }) => (success ? theme.colors.green.mid : theme.colors.red.mid)};
   border-radius: 999px;
   width: 14px;
   height: 14px;
