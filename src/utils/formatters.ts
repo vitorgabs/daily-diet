7e5793b68249609bb21cdc1904a26ca129dd0f42
Vitor Gabriel <vitorgabsg@gmail.com>
2025-03-13 16:12:51 -0300

    fix: remove unnecessary decimal places

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,7 +1,6 @@
 export function formatToDecimalPercentage(value: number) {
   const formattedValue = Intl.NumberFormat('pt-br', {
     style: 'decimal',
-    minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(value)
 
